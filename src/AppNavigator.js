598390f02a1d506ea4db823348a0eb6b97dfd79f
alekseibykov <gogohandz@gmail.com>
2019-08-14 16:32:17 +0300

    Add calendar picker and calendar screen

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -4,11 +4,13 @@ import { connect } from 'react-redux';
 
 import HomeScreen from './components/HomeScreen';
 import RegistrationScreen from './components/RegistrationScreen';
+import CalendarScreen from './components/CalendarScreen';
 import withAuthentication  from './components/Session/withAuthentication';
 
 const MainNavigator = createStackNavigator({
   Home: {screen: HomeScreen},
   Registration: {screen: RegistrationScreen},
+  Calendar: {screen: CalendarScreen},
 },
 {
   headerMode: 'none',
